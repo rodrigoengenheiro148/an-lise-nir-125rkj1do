a99f392d686fe9564093b06714f94313ac00aa11
Skip <skip@usecurling.com>
2026-03-23 23:49:55 +0000

    Auto-commit before sync

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -2,5 +2,5 @@ export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers':
-    'authorization, x-client-info, x-supabase-client-platform, apikey, content-type',
+    'authorization, x-client-info, apikey, content-type',
 }
